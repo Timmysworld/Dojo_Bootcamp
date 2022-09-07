5fb477f22eeaec6b56cf6a6950e921458e0039d7
TimothySingleton <t.singleton89@hotmail.com>
2022-09-07 12:51:10 -0600

    Updated code added class and static methods

--- Python/Assignments/BankAccount.py
+++ Python/Assignments/BankAccount.py
@@ -1,44 +1,63 @@
 class BankAccount:
     # new class attribute - a list of all the accounts!
-    # all_accounts = []
+    all_accounts = []
 
     def __init__(self, int_rate, balance): 
         self.int_rate = int_rate
         self.balance = balance
-        # BankAccount.all_accounts.append(self)
-        return None
+        BankAccount.all_accounts.append(self)
+
 
     def deposit(self, amount):
         self.balance += amount
         return self
 
     def display_account_info(self):
-        print(f" Account Balance: ${self.balance}")
+        print(f"Account Balance: ${self.balance}")
         return self
 
     def withdraw(self, amount):
-        if self.balance > amount:
+        if BankAccount.can_withdraw(self.balance,amount): # with Static method all bank accounts can be checked for insufficient funds.
             self.balance -= amount
-            # print(f"You've withdrawn: $ ")
+            print(f"You've withdrawn: ${amount} ")
         else: 
             print(f"Insufficient Funds ${self.balance}")
         return self
 
     def yield_interest(self):
-        if self.balance == self.balance:
-            self.balance += 0.1
-            print(f"Your new balance is: ${self.balance}")
-        else:
-            # if self.balance:
+        if self.balance == 0:
                 print(f"Sorry your account is Negative")
+        elif self.balance == self.balance:
+            self.balance += 0.10
+            print(f"Your new balance is: ${self.balance}")
         return self
+        
+# static methods have no access to any attribute
+# only to what is passed into it
+    @staticmethod
+    def can_withdraw(balance,amount):
+        if (balance - amount) < 0:
+            return False
+        else:
+            return True
 
-checking = BankAccount(0.1, 500.00)
-checking.display_account_info().withdraw(100).yield_interest()
+    @classmethod 
+    def total_accounts(cls):
+        sum = 0
+        for account in cls.all_accounts:
+            sum += account.balance
+        return sum
+
+checking = BankAccount(0.10, 500.00)
+checking.display_account_info().withdraw(100.00).yield_interest()
 
 savings = BankAccount(0.1, 1000.00)
-savings.display_account_info().withdraw(1000).yield_interest()
+savings.display_account_info().withdraw(100.00).yield_interest()
+
+# calling the Method total_accounts (class.method)
+print(f"Total Cash: ${BankAccount.total_accounts()}")
+# x = BankAccount.total_accounts()
+# print(x)
 
 # QUESTIONS TO ASK:
-# Can not seem to get the logic to test if self.balance is a negative number or see that account has insufficient funds and can not yield interest on account.
 # want to figure out how to show the amount withdrawn in print statement 
