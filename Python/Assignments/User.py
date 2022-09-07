4638235c28d5c4872bec677b36aa5ff4d5a38026
TimothySingleton <t.singleton89@hotmail.com>
2022-09-07 09:42:23 -0600

    miscellaneous commit

--- Python/Assignments/User.py
+++ Python/Assignments/User.py
@@ -3,6 +3,11 @@ from tkinter import FALSE
 
 # For this assignment you will create the user class and add a couple methods!
 
+
+#NOTES:
+# Each instance method created needs to have self as a parameter.
+# self has all the attributes created defined in the __init__ 
+
 class User:
     def __init__(self,first_name,last_name,email,age,is_rewards_member,gold_card_points):
         self.first_name = first_name
@@ -12,26 +17,28 @@ class User:
         self.is_rewards_member = is_rewards_member
         self.gold_card_points = gold_card_points
         self.member_status = "Entry"
+        return None
 
     def display_info(self):
         print(f"First Name: {self.first_name}")
         print(f"Last Name: {self.last_name}")
-        print(f"Email:{self.email}")
-        print(f"Age:{self.age}")
+        print(f"Email: {self.email}")
+        print(f"Age: {self.age}")
         print(f"Rewards Member: {self.is_rewards_member}")
-        print(f"Gold Points:{self.gold_card_points}")
-        print(f"Member Status:{self.member_status}")
+        print(f"Gold Points: {self.gold_card_points}")
+        print(f"Member Status: {self.member_status}")
         print(f"========================================")
+        return self
 
     def enroll(self):
         if self.is_rewards_member == False:
             self.is_rewards_member = True 
             self.gold_card_points = 200 
             self.member_status = "Bronze"
-            return True
         else:
             print("Already a Member")
-            return False
+        return self
+
 
     def bonus_points(self):
         if self.gold_card_points == 200:
@@ -42,8 +49,9 @@ class User:
                 self.member_status ="Silver"
         elif self.gold_card_points > 200:
             self.gold_card_points += 25
-            if self.gold_card_points > 650:
+        elif self.gold_card_points > 650:
                 self.member_status = "Gold"
+        return self
 
     def spend_points(self, amount):
         if (amount < self.gold_card_points):
@@ -52,14 +60,15 @@ class User:
             print(f"Watch your spending {self.first_name}")
         else:
             print(f"Sorry you don't have enough points to spend {self.first_name}")
-
+        return self
                 
             
 
 Anonymous_User = User("John","Doe","John.D@hotmail.com",32,False,0)
-Anonymous_User2 = User("Tiffany","New","T.New@gmail.com",27,False,0)
+Anonymous_User2 = User("Tiffany","New","T.New@gmail.com",27,True,1650)
 Anonymous_User.display_info().enroll().display_info().spend_points(200).display_info()
 Anonymous_User2.display_info().enroll().display_info().spend_points(200).display_info()
 
 
 # Update Code Logic for if user is a member but have no points 
+# Need to fix bonus points logic
