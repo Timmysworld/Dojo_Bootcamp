8a1bd5ee90f0bf734d2c829f6a6079b05bd6fe7e
TimothySingleton <t.singleton89@hotmail.com>
2022-09-16 09:14:11 -0600

    intial demo testing of flask

--- Python/flask/demo/server.py
+++ Python/flask/demo/server.py
@@ -1,13 +1,17 @@
-from unicodedata import name
-from flask import Flask, render_template
+from flask import Flask, render_template # Import Flask to allow us to create our app
 
 
+app = Flask (__name__) # Create a new instance of the Flask class called "app" You will need this line in every application you build with Flask.
 
-app = Flask (__name__)
-
-@app.route("/")
+@app.route("/") # The "@" decorator associates this route with the function immediately following  # @app.route("/home/_____") anything after '/hello/' gets passed as a variable 'name'
 def index():
-    return render_template("index.html", name="Timothy")
+    return render_template("index.html", name="Timothy") # Return the string 'Hello World!' as a response
 
-if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+@app.route('/users/<username>/<id>')
+def show_user_profile(username, id):
+    print(username)
+    print(id)
+    return "username: " + username + ", id: " + id 
+
+if __name__ == "__main__":  # Ensure this file is being run directly and not from a different module  
+    app.run(debug=True, port=8000)  # Run the app in debug mode. mac users must use port 8000
