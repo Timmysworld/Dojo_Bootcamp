ca4d37973ead1fd3eb3027e774bca3643970997c
TimothySingleton <t.singleton89@hotmail.com>
2022-09-22 19:26:37 -0600

    Testing database connections

---
+++ Python/flask_mysql/db_connection/first_flask-mysql/flask_app/models/friend.py
@@ -0,0 +1,36 @@
+# import the function that will return an instance of a connection
+from flask_app.config.mysqlconnection import connectToMySQL
+# from mysqlconnection import connectToMySQL
+# model the class after the friend table from our database
+class Friend:
+    def __init__( self , data ):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.occupation = data['occupation']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+        # Now we use class methods to query our database
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM friends;"
+        # make sure to call the connectToMySQL function with the schema you are targeting.
+        results = connectToMySQL('first_flask').query_db(query)
+        # Create an empty list to append our instances of friends
+        friends = []
+        # Iterate over the db results and create instances of friends with cls.
+        for friend in results:
+            friends.append( cls(friend) )
+        return friends
+
+    @classmethod
+    def save(cls, data ):
+        query = "INSERT INTO friends ( first_name , last_name , occupation , created_at, updated_at ) VALUES ( %(fname)s , %(lname)s , %(occ)s , NOW() , NOW() );"
+        # data is a dictionary that will be passed into the save method from server.py
+        return connectToMySQL('first_flask').query_db( query, data )
+            
+
+
+
+# SOMETHING TO REMEMBER # 
+# To Python, the query variable is just a string, and the data dictionary just another variable. But when passed to our MySQL server through our PyMySQL connection, our MySQL database understands the string as a query it can execute!
