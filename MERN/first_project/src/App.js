c0acb69656d3b72fddf15ef2b5bb6b272b9eb7c3
TimothySingleton <t.singleton89@hotmail.com>
2022-10-26 15:25:30 -0600

    first react project

---
+++ MERN/first_project/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Hello Dojo!</h1>
+      </header>
+      <main>
+      <h3 className="App-list-title"> Things I need to do:</h3>
+        <ul className="App-list">
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run A Marathon</li>
+          <li>Feed the Dogs</li>
+        </ul>
+      </main>
+        
+    </div>
+  );
+}
+
+export default App;
