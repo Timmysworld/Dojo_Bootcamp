6d3124913a991fbafa3bad247d9fa022110a17c7
TimothySingleton <t.singleton89@hotmail.com>
2022-11-13 12:01:10 -0700

    pokemon_api : fetch

---
+++ MERN/Assignments/pokemon_api/src/App.js
@@ -0,0 +1,50 @@
+import './App.css';
+import {useEffect, useState} from 'react'
+
+
+function App() {
+    const [pokemon, setPokemon] = useState([])
+    
+    useEffect(()=>{
+      fetch('https://pokeapi.co/api/v2/pokemon?limit=807&offset=0')
+      // .then(res =>res.json())
+      // .then(res =>setPokemon(res.result))
+      .then((res)=>{
+        return res.json()
+      })
+      .then((res)=>{
+        console.log(res.results)
+        setPokemon(res.results)
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+    },[]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> POKEMON</h1>
+        <p>Gotta catch 'em all</p>
+      </header>
+
+      <>
+      {
+        pokemon.map((pName,index)=>(
+          <ul>
+            <li>{pName.name}</li>
+          </ul>
+        ))
+      }
+
+      {/* {pokemon.length > 0 && pokemon.map((pokemon, index)=>{
+                return (<div key={index}>{pokemon.name}</div>)
+            })} */}
+
+      </>
+
+    </div>
+  );
+}
+
+export default App;
