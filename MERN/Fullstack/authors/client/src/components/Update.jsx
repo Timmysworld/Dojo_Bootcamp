cfd4f4b77f6727abac0f17ac917d0147b0190b83
TimothySingleton <t.singleton89@hotmail.com>
2022-12-12 23:40:01 -0700

    debugging validations

--- MERN/Fullstack/authors/client/src/components/Update.jsx
+++ MERN/Fullstack/authors/client/src/components/Update.jsx
@@ -2,15 +2,17 @@ import React, {useEffect, useState} from 'react'
 import axios from 'axios';
 import {useNavigate, useParams} from 'react-router-dom';
 
-const Update = (authorName, setAuthorName) => {
+const Update = (props) => {
     const {id} = useParams();
 
     const [name,setName] = useState();
+    const [errors, setErrors] = useState({});
     const navigate = useNavigate();
 
     useEffect(()=>{
         axios.get(`http://localhost:8000/api/author/${id}`)
             .then(res => {
+                console.log(res.data)
                 setName(res.data.name);
             })
             .catch(err => console.log(err))
@@ -23,24 +25,33 @@ const Update = (authorName, setAuthorName) => {
         })
             .then(res=> {
                 console.log(res);
+                console.log(res.data)
                 navigate('/');
             })
-            .catch(err=>console.log(err))
+            .catch((err) => {
+                console.log(err)
+                console.log(err.response.data.error.errors);
+                setErrors(err.response.data.error.errors);
+            });
+            // .catch(err=>console.log(err))
     }
 
     return (
         <div>
             <h1>Update Author</h1>
             <form onSubmit={updateOneAuthor}>
-                <p>
+                <div>
                     <label>Name:</label><br/>
+                    {errors.name ?
+                    <p style={{color:"red"}}>{errors.name.message}</p>
+                    : null}
                     <input type="text"
                     name="name"
                     value={name}
                     onChange ={(e)=>{
                         setName(e.target.value)
                     }}/>
-                </p>
+                </div>
                 <input type="submit" />
             </form>
         </div>
