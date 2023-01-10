182fb8669e2b0c34e4622fc39e7955433c7236ac
TimothySingleton <t.singleton89@hotmail.com>
2023-01-10 16:42:11 -0700

    init commit

---
+++ Java/Assignments/Greetings/Test.java
@@ -0,0 +1,13 @@
+// File name must be the same as the class name
+// Every file IS a class
+public class Test {
+    // The file or calss with the exact method signature below 
+    // is always the launching point of the application 
+    // you cannot run a Java program without it.
+    public static void main(String[] args){
+        //for now , all the code goes here!
+        System.out.println("Hello World");
+    }
+}
+
+//public static void are just key words
