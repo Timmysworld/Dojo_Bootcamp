73110c4f7ba54eb40197145b9b195a3ebc0845a8
TimothySingleton <t.singleton89@hotmail.com>
2023-01-31 11:23:22 -0700

    initial commit

---
+++ DaikichiPathVariables/src/main/java/com/timmysworld/DaikichiPathVariables/controllers/MainController.java
@@ -0,0 +1,41 @@
+package com.timmysworld.DaikichiPathVariables.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/daikichi")
+public class MainController {
+	
+	@GetMapping("")
+	public String index() {
+		return "Welcome!";
+	}
+	
+	@GetMapping("/today")
+	public String today() {
+		return "Today you will find luck in all your endeavors!";
+	}
+	
+	@GetMapping("/tomorrow")
+	public String tomorrow() {
+		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+	//PATH VARIABLES
+	@GetMapping("/travel/{location}")
+	public String travel(@PathVariable("location") String location) {
+		return "Congratulations! You will soon travel to " + location + "!";
+	}
+	
+	@GetMapping("/lotto/{id}")
+	public String lotto(@PathVariable("id")int id) {
+		if (id % 2 == 0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers";
+		}
+		return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+		
+	}
+}
